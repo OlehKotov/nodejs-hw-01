022036bcf0b561cfe3bc89919daadaf859bf965e
OlehKotov <mail.oleg456@gmail.com>
2024-05-19 09:46:10 +0300

    Update

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -4,7 +4,7 @@ import fs from 'fs/promises';
 export const getAllContacts = async () => {
     try {
         const data = await fs.readFile(PATH_DB, 'utf8');
-        console.log('Вміст файлу:', data);
+        return data;
       } catch (err) {
         console.error('Помилка читання файлу:', err);
       }
